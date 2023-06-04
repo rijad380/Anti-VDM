874ef1ece766319b37b380d2a8cd49262ec4ecdf
rijad380 <74475478+rijad380@users.noreply.github.com>
2023-06-04 23:01:03 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'Thekuca#0001'
+description 'Anti VDM'
+version '1.0.0'
+
+client_script 'cl.lua'
+server_script 'sv.lua'
+dependency '/assetpacks'
